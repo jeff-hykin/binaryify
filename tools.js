5274098631b00f4a67a0e80dd2e89283b43db972
Jeff Hykin <jeff.hykin@gmail.com>
2023-08-24 17:22:31 -0500

    -

--- tools.js
+++ tools.js
@@ -179,7 +179,7 @@ export async function binaryify({pathToBinary, pathToBinarified}) {
     await FileSystem.write({
         path: pathToBinarified,
         data: `
-            import { stringToBytes } from "https://deno.land/x/binaryify@2.2.0.2/tools.js"
+            import { stringToBytes } from "https://deno.land/x/binaryify@2.2.0.3/tools.js"
             export default stringToBytes(${stringToBacktickRepresentation(bytesToString(await Deno.readFile(pathToBinary)))})
         `,
     })
