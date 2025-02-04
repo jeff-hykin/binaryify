f2371a2c9ae1eec977c161b50bdfcfe10c0ed809
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-04 10:04:25 -0600

    remove joke semicolons

--- binaryify.js
+++ binaryify.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S deno run --allow-all
-import { cyan, white, yellow, green, red } from "https://deno.land/x/quickr@0.6.47/main/console.js"                                                                                                                                                                                                                                                                                                                                                                          ;
-import { binaryify } from "./binaryify_api.js"                                                                                                                                                                                                                                                                                                                                                                                                                               ;
-import { parseArgs, flag, required, initialValue } from "https://deno.land/x/good@1.7.1.1/flattened/parse_args.js"                                                                                                                                                                                                                                                                                                                                                           ;
-import { didYouMean } from "https://deno.land/x/good@1.7.1.1/flattened/did_you_mean.js"                                                                                                                                                                                                                                                                                                                                                                                      ;
-import version from "./version.js"                                                                                                                                                                                                                                                                                                                                                                                                                                       ;
+import { cyan, white, yellow, green, red } from "https://deno.land/x/quickr@0.6.47/main/console.js"
+import { binaryify } from "./binaryify_api.js"
+import { parseArgs, flag, required, initialValue } from "https://deno.land/x/good@1.7.1.1/flattened/parse_args.js"
+import { didYouMean } from "https://deno.land/x/good@1.7.1.1/flattened/did_you_mean.js"
+import version from "./version.js"
 
 const argsInfo = parseArgs({
     rawArgs: Deno.args,
@@ -15,39 +15,39 @@ const argsInfo = parseArgs({
     allowNameRepeats: true,
     valueTransformer: JSON.parse,
     isolateArgsAfterStopper: true,
-})                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;
+})
 didYouMean({
     givenWords: Object.keys(argsInfo.implicitArgsByName).filter((each) => each.startsWith(`-`)),
     possibleWords: Object.keys(argsInfo.explicitArgsByName).filter((each) => each.startsWith(`-`)),
     autoThrow: true,
     suggestionLimit: 1,
-})                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;
-const { version: showVersion, help: showHelp } = argsInfo.explicitArgsByName                                                                                                                                                                                                                                                                                                                                                                                                  ;
-const filePaths = argsInfo.argsAfterStopper                                                                                                                                                                                                                                                                                                                                                                                                                                   ;
+})
+const { version: showVersion, help: showHelp } = argsInfo.explicitArgsByName
+const filePaths = argsInfo.argsAfterStopper
 
-if (showVersion) {                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;
+if (showVersion) {
     console.log(version)
-    Deno.exit(0)                                                                                                                                                                                                                                                                                                                                                                                                                                                              ;
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ;
+    Deno.exit(0)
+}
 
-if (filePaths.length == 0 || showHelp) {                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
+if (filePaths.length == 0 || showHelp) {
     console.log(`
         To binaryify a file (or multiple) just give this command some arguments
         Instructions will be given on how to handle the binaryified-file after
 
         ex:
             ${green`binaryify`} -- ./your_file.something
-    `.replace(/\n        /g, "\n"))                                                                                                                                                                                                                                                                                                                                                                                                                                           ;
-} else {                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
-    const namesAndStuff = await Promise.all(filePaths.map((eachPath) => binaryify({ pathToBinary: eachPath })))                                                                                                                                                                                                                                                                                                                                                               ;
+    `.replace(/\n        /g, "\n"))
+} else {
+    const namesAndStuff = await Promise.all(filePaths.map((eachPath) => binaryify({ pathToBinary: eachPath })))
     console.log(`
         // paths have been generated!
         // add this wherever you need it now:
 
         // NOTE: this is auto-updating!
         // if you change the YOUR_FILE.wasm, YOUR_FILE.wasm.binaryified.js will change too!
-    `.replace(/\n        /g, "\n"))                                                                                                                                                                                                                                                                                                                                                                                                                                           ;
-    for (let [realNameSuggestion, newPath] of namesAndStuff) {                                                                                                                                                                                                                                                                                                                                                                                                                ;
-        console.log(`${cyan`import`} ${yellow("uint8ArrayFor" + realNameSuggestion)} ${cyan`from`} ${green(JSON.stringify(newPath))}`)                                                                                                                                                                                                                                                                                                                                        ;
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                         ;
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ;
+    `.replace(/\n        /g, "\n"))
+    for (let [realNameSuggestion, newPath] of namesAndStuff) {
+        console.log(`${cyan`import`} ${yellow("uint8ArrayFor" + realNameSuggestion)} ${cyan`from`} ${green(JSON.stringify(newPath))}`)
+    }
+}
